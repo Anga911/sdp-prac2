011f282627fe6cca2ffd53eef8c4495c26b0be74
Anga911 <g23p2320@campus.ru.ac.za>
2025-04-14 01:18:43 +0200

    Anga: Implemented Task1 in SimpleFunctions

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,4 +4,29 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
+
+    /*
+    Anga: Task 1 
+    Below is a method which accepts lists of 
+    integers a and b, and adds up the elements 
+    from a whose indexes are specified by b.  
+    If any index in b is out-of-range for a, that index is ignored :) 
+    */
+
+    public static int Task1(int[] a, int[] b){
+
+        int acc = 0;
+
+        for (int i = 0; i < b.length; i++){
+            
+            if (b[i] < 0 || b[i] >= a.length){
+                continue;
+            }else{
+                acc += a[b[i]];
+            }
+        }
+        return acc;
+    }
 }
+
+
