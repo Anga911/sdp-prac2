d384568247017dbbfed8858a51b25ee98bdddf99
Anesipho Nkonkobe <G23N7615@ict.ru.ac.za>
2025-04-14 23:02:23 +0200

    Completed Task2

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,4 +4,20 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
+
+    /*Task2 written by Anesipho
+    This method removes the first character from every element in a list
+    and returns the modified list*/ 
+    public static List<String> Task2(List<String> aList){
+        List<String> output = new ArrayList<>();
+
+        for (String str : aList){
+            if (str.length() > 1){
+                output.add(str.substring(1));
+            } else if (str.length() == 1){
+                continue;
+            }
+        }
+        return output;
+    }
 }
