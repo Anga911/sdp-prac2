788deeb6c1808f045e405954ddc22a39bfa7184a
Anga911 <g23p2320@campus.ru.ac.za>
2025-04-15 09:12:23 +0200

    Anga: Added test cases for task 2

--- app/src/test/java/sdp/prac2/AppTest.java
+++ app/src/test/java/sdp/prac2/AppTest.java
@@ -6,5 +6,26 @@ package sdp.prac2;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class AppTest {
+
+    /*
+     Anga: Test case for Task 2
+     */
+
+    @Test
+    public void testTask2(){
+
+        List<String> test = Arrays.asList("No", "Yes", "GG", "Maybe", "", "L");
+        List<String> outcome = Arrays.asList("o", "es", "G", "aybe", "");
+
+        List<String> test2 = Arrays.asList("Apple", "Banana", "Grapes", "Kiwi", "Tomato");
+        List<String> outcome2 = Arrays.asList("pple", "anana", "rapes", "iwi", "omato");
+
+        assertEquals(outcome, SimpleFunctions.Task2(test));
+        assertEquals(outcome2, SimpleFunctions.Task2(test2));
+    }
 }
