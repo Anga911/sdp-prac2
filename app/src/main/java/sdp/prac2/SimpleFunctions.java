719b1f15a691219a7fb66991a92e5a40ac80ce36
Anga911 <g23p2320@campus.ru.ac.za>
2025-04-15 09:50:41 +0200

    Anga: Modified Task1

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -27,46 +27,6 @@ public class SimpleFunctions {
         }
         return acc;
     }
-
-    /*Task2 written by Anesipho
-    This method removes the first character from every element in a list
-    and returns the modified list*/ 
-    public static List<String> Task2(List<String> aList){
-        List<String> output = new ArrayList<>();
-
-        for (String str : aList){
-            if (str.length() > 1){
-                output.add(str.substring(1));
-            } else if (str.length() == 1){
-                continue;
-            }
-        }
-        return output;
-    }
-
-    /* Task6, which rounds up each number in the provided integer list to the next-highest multiple of 100. 
-    If a number is already a multiple of 100, it is left unchanged. 
-    Do not modify the list that is passed; instead, return a new list.
-    param lst: the original Integer List
-    
-    Intialises newLst as original List can not be modified
-    uses a for loop to iterate through orginal list.
-    uses ceiling function to round up numbers divided by 100.0 to prevent integer division
-    Once the decimals are rounded up it is multiplied by 100 to get proper number
-    
-     * 
-     */
-    public List<Integer> listRoundUp(List<Integer> lst){ //task 6 By Karabo Ntsie!
-        List<Integer> newLst = new ArrayList<Integer>();
-
-        for(Integer num: lst){
-            Integer roundedUp = (int) Math.ceil(num/100.0)*100;
-            newLst.add(roundedUp);
-        }
-
-        return newLst;
-    }
-    
 }
 
 
